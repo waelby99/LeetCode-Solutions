0d8599006423e7463516282ca8f4a8f82312bccc
Ben Youssef Wael <52159045+waelby99@users.noreply.github.com>
2026-09-14 15:08:49 +0100

    Time: 0 ms (100%), Space: 41.8 MB (95.25%) - LeetHub

---
+++ 2481-minimum-cuts-to-divide-a-circle/2481-minimum-cuts-to-divide-a-circle.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numberOfCuts(int n) {
+        int res = 0;
+        if ( n == 1) return res;
+        if ( n == 3) return n;
+        if ( n % 2 == 0){
+            res = n / 2;
+        }
+        else if ( n % 2 != 0){
+            res = n;
+        }
+        return res;
+    }
+}
