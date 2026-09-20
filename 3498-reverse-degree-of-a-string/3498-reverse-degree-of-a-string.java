96f4940bac94c4d1c312ee7051fe7e466dcbdd6b
Ben Youssef Wael <52159045+waelby99@users.noreply.github.com>
2026-09-20 18:08:07 +0100

    Time: 1 ms (100%), Space: 44 MB (67.44%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+        for( int i = 0 ; i < s.length() ; i++ ){
+            sum += (('z' + 1 - s.charAt(i))*(i + 1));
+        } 
+        return sum; 
+    }
+}
