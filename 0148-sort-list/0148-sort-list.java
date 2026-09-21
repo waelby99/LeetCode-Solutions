b4032c844c70bfc7b3774821073842a39dfe7a2f
Ben Youssef Wael <52159045+waelby99@users.noreply.github.com>
2026-09-21 10:13:08 +0100

    Time: 14 ms (21.56%), Space: 64.8 MB (5.29%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,6 +9,7 @@
  * }
  */
 class Solution {
+    //very space and time complexity needs to be re-done 
     public ListNode sortList(ListNode head) {
         ArrayList<Integer> al = new ArrayList<>();
         while( head != null ){
