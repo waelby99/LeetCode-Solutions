601f56249854b04b462811dc9b05ed2eb20bbdc5
Ben Youssef Wael <52159045+waelby99@users.noreply.github.com>
2026-09-23 11:31:55 +0100

    Time: 3 ms (13.31%), Space: 44.6 MB (8.24%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,83 +1,109 @@
 class Solution {
     public String addBinary(String a, String b) {
-        //finalllllyyyyyyyyyyy 
+        
+        /*Solution 2 (more optimized than solution 1) */
         StringBuilder sb = new StringBuilder();
         int rest = 0;
-        if (a.length() > b.length()) {
-            while (a.length() > b.length()) {
-                b = "0" + b;
-            }
+
+        while (a.length() > b.length()) {
+            b = "0" + b;
         }
-        if (a.length() < b.length()) {
-            while (a.length() < b.length()) {
-                a = "0" + a;
-            }
+
+        while (a.length() < b.length()) {
+            a = "0" + a;
         }
 
         int index = a.length() - 1;
 
         while (index >= 0) {
 
+            char x = a.charAt(index);
+            char y = b.charAt(index);
+
             if (rest == 0) {
-                if (a.charAt(index) == '0' && b.charAt(index) == '0') {
-                    sb.insert(0, "0");
-                } else if ((a.charAt(index) == '1' && b.charAt(index) == '0')
-                        || (a.charAt(index) == '0' && b.charAt(index) == '1')) {
-                    sb.insert(0, "1");
-                } else if (a.charAt(index) == '1' && b.charAt(index) == '1') {
-                    sb.insert(0, "0");
+
+                if (x == '0' && y == '0') {
+                    sb.insert(0, '0');
+
+                } else if (x != y) {
+                    sb.insert(0, '1');
+
+                } else {
+                    sb.insert(0, '0');
                     rest = 1;
                 }
-            } else if (rest == 1) {
-                if (a.charAt(index) == '0' && b.charAt(index) == '0') {
-                    sb.insert(0, "1");
+
+            } else {
+
+                if (x == '0' && y == '0') {
+                    sb.insert(0, '1');
                     rest = 0;
-                } else if ((a.charAt(index) == '1' && b.charAt(index) == '0')
-                        || (a.charAt(index) == '0' && b.charAt(index) == '1')) {
-                    sb.insert(0, "0");
-                    rest = 1;
-                } else if (a.charAt(index) == '1' && b.charAt(index) == '1') {
-                    sb.insert(0, "1");
-                    rest = 1;
+
+                } else if (x != y) {
+                    sb.insert(0, '0');
+
+                } else {
+                    sb.insert(0, '1');
                 }
             }
 
             index--;
         }
-        if ( rest == 1){
-            sb.insert(0, "1");
+
+        if (rest == 1) {
+            sb.insert(0, '1');
         }
+
         return sb.toString();
     }
 }
 
-// int n1 = 0;
-// int n2 = 0;
-// for (int i = 0; i < a.length(); i++) {
-//     int n = a.charAt(i) - '0';
-//     n1 = n1 + (n * (int) Math.pow(10, a.length() - 1 - i));
-// }
-// for (int i = 0; i < b.length(); i++) {
-//     int n = b.charAt(i) - '0';
-//     n2 = n2 + (n * (int) Math.pow(10, b.length() - 1 - i));
-// }
-// String s = Integer.toString(n1 + n2);
-// String res = "";
+/* Solution 1 */
+//finalllllyyyyyyyyyyy 
+// StringBuilder sb = new StringBuilder();
 // int rest = 0;
-// for (int i = 0; i < s.length(); i++) {
-//     if (s.charAt(i) == '2') {
-//         s = s.substring(0, i) + '0' + s.substring(i + 1);
-//         rest = 1;
+// if (a.length() > b.length()) {
+//     while (a.length() > b.length()) {
+//         b = "0" + b;
 //     }
-//     if (s.charAt(i) == '1' && rest == 1) {
-//         s = s.substring(0, i) + '0' + s.substring(i + 1);
-//         rest = 1;
-//     }
-
-//     if (s.charAt(i) == '0' && rest == 1) {
-//         s = s.substring(0, i) + '1' + s.substring(i + 1);
-
-//     }
-
 // }
-// return s;
+// if (a.length() < b.length()) {
+//     while (a.length() < b.length()) {
+//         a = "0" + a;
+//     }
+// }
+
+// int index = a.length() - 1;
+
+// while (index >= 0) {
+
+//     if (rest == 0) {
+//         if (a.charAt(index) == '0' && b.charAt(index) == '0') {
+//             sb.insert(0, "0");
+//         } else if ((a.charAt(index) == '1' && b.charAt(index) == '0')
+//                 || (a.charAt(index) == '0' && b.charAt(index) == '1')) {
+//             sb.insert(0, "1");
+//         } else if (a.charAt(index) == '1' && b.charAt(index) == '1') {
+//             sb.insert(0, "0");
+//             rest = 1;
+//         }
+//     } else if (rest == 1) {
+//         if (a.charAt(index) == '0' && b.charAt(index) == '0') {
+//             sb.insert(0, "1");
+//             rest = 0;
+//         } else if ((a.charAt(index) == '1' && b.charAt(index) == '0')
+//                 || (a.charAt(index) == '0' && b.charAt(index) == '1')) {
+//             sb.insert(0, "0");
+//             rest = 1;
+//         } else if (a.charAt(index) == '1' && b.charAt(index) == '1') {
+//             sb.insert(0, "1");
+//             rest = 1;
+//         }
+//     }
+
+//     index--;
+// }
+// if ( rest == 1){
+//     sb.insert(0, "1");
+// }
+// return sb.toString();
